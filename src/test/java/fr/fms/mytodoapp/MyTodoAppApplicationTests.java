64fff83834c898825757866e783df5d2f56b2515
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-04-03 16:32:52 +0200

    first commit

---
+++ src/test/java/fr/fms/mytodoapp/MyTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.mytodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
