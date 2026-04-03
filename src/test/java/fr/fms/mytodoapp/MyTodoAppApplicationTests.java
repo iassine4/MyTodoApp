84d3d1291e65b601810ea32feacd4321072e1240
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-04-03 16:37:04 +0200

    initial commit

--- src/test/java/fr/fms/mytodoapp/MyTodoAppApplicationTests.java
+++ src/test/java/fr/fms/mytodoapp/MyTodoAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyTodoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
