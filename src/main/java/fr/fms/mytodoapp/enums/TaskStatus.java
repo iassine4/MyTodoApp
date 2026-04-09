2526b2c4284173eac3d3f0abd5945a6ecc9d6619
ZRIBA Yassine <ia.ssine3325@gmail.com>
2026-04-09 16:51:19 +0200

    add french labels to task status enum

--- src/main/java/fr/fms/mytodoapp/enums/TaskStatus.java
+++ src/main/java/fr/fms/mytodoapp/enums/TaskStatus.java
@@ -3,26 +3,52 @@ package fr.fms.mytodoapp.enums;
 /**
  * Représente les statuts possibles d'une tâche dans l'application.
  *
- * <p>Un enum permet de limiter les valeurs autorisées
- * à une liste fermée et connue à l'avance.</p>
- *
+ * <p>Chaque statut possède :</p>
  * <ul>
- *     <li>TO DO : la tâche est à faire</li>
- *     <li>IN_PROGRESS : la tâche est en cours</li>
- *     <li>DONE : la tâche est terminée</li>
+ *     <li>une valeur technique utilisée dans le code et la base de données</li>
+ *     <li>un libellé utilisateur affiché dans les vues</li>
  * </ul>
  *
- * @author - Yassine ZRIBA
+ * @author Yassine ZRIBA
  * @version 1.0
  */
 public enum TaskStatus {
 
-    /** Tâche à faire. */
-    TODO,
+    /**
+     * Tâche à faire.
+     */
+    TODO("À faire"),
 
-    /** Tâche en cours de réalisation. */
-    IN_PROGRESS,
+    /**
+     * Tâche en cours.
+     */
+    IN_PROGRESS("En cours"),
 
-    /** Tâche terminée. */
-    DONE
+    /**
+     * Tâche terminée.
+     */
+    DONE("Terminée");
+
+    /**
+     * Libellé lisible affiché à l'utilisateur.
+     */
+    private final String label;
+
+    /**
+     * Construit un statut avec son libellé utilisateur.
+     *
+     * @param label le libellé affiché dans les vues
+     */
+    TaskStatus(String label) {
+        this.label = label;
+    }
+
+    /**
+     * Retourne le libellé utilisateur du statut.
+     *
+     * @return le libellé du statut
+     */
+    public String getLabel() {
+        return label;
+    }
 }
